64e5342ec1cdddaeb573c43abf86786aac78f74e
Siya Sibeko <siyandasibeko00@gmail.com>
2025-12-10 21:42:12 +0200

    Add files via upload

--- finance_calculators.py
+++ finance_calculators.py
@@ -8,21 +8,24 @@ investment_calculation = input("Enter either Investment or Bond from the menu ab
 
 #One of the three format to be used for investment to be calculated
 if investment_calculation == "Investment" or  investment_calculation == "INVESTMENT" or investment_calculation == "investment":
-    P = int(input("Deposit:"))
+    P = int(input("How much are you depositing:"))
     r = int(input("Interest rate:"))
     t = int(input("Number of years:"))
     interest = input("Simple or Compound Interest:") #User to select to calculate one of the two options
-    if interest == "Simple":
-        interest_simple = print(P*(1 + ((r/100)*t)))
-    elif interest == "Compound":
-        interest_compound = print((P*(1 + (r/100)),t))
+    if interest == "Simple" or interest == "simple" or interest == "SIMPLE":
+        interest_simple = P*(1 + ((r/100)*t))
+        print("Total investment (simple):") + print(round(interest_simple,2))
+    elif interest == "Compound" or interest == "compound" or interest == "COMPOUND":
+        interest_compound = P*math.pow((1+(r/100)),t)
+        print("Total investment (compound):") + print(round(interest_compound,2))
 
 #One of the three formats to be used for bond repayments to be calculated
 elif investment_calculation == "Bond" or investment_calculation == "BOND" or investment_calculation == "bond":
-    P = int(input("Present Value:"))
+    P = int(input("Initial amount for investment (Present Value):"))
     i = int(input("Interest rate:"))
     n = int(input("Number of months:"))
-    bond_repayment = print((((i/100)/12) * P)/(1 - (1 + (i/100)/12)**(-n)))
+    bond_repayment = (((i/100)/12) * P)/(1 - (1 + (i/100)/12)**(-n))
+    print("Total bond repayments:") + print(round(bond_repayment,2))
 
 else:
     print("Sorry! Please enter correct option from above menu:")
