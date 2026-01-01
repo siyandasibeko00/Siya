81364808e9c4b2f67fa95cdcc72585f8a98dcf4a
Siya Sibeko <siyandasibeko00@gmail.com>
2026-01-01 23:28:52 +0200

    Add files via upload

--- debugging_task.py
+++ debugging_task.py
@@ -1,17 +1,17 @@
 # Function to print dictionary values given the keys
 def print_values_of(dictionary, keys):
     for key in keys:
-        print(dictionary[key])
+        print(dictionary[key]) #replaced k with key in square brackets
 
 # Print dictionary values from simpson_catch_phrases
 simpson_catch_phrases = {"lisa": "BAAAAAART!", 
                          "bart": "Eat My Shorts!", 
                          "marge": "Mmm~mmmmm", 
-                         "homer": "d'oh!", 
+                         "homer": "d'oh!", #replaced single quotation with double quotation
                          "maggie": "(Pacifier Suck)"
                          }
 
-print_values_of(simpson_catch_phrases, ['lisa', 'bart', 'homer'])
+print_values_of(simpson_catch_phrases, ['lisa', 'bart', 'homer']) #placed square brackets to form list between lisa and homer
 
 '''
     Expected console output:
