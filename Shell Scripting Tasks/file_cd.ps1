cae721b0d2453a3a773c13a9fb27278821809aa3
Siya Sibeko <siyandasibeko00@gmail.com>
2026-02-08 21:11:46 +0200

    Add files via upload

--- Shell Scripting Tasks/file_cd.ps1
+++ Shell Scripting Tasks/file_cd.ps1
@@ -1,8 +1,10 @@
-$path1 = New-Item -Path "C:\Users\user\Desktop\$folder1" -ItemType Directory
-$path2 = New-Item -Path "C:\Users\user\Desktop\$folder2" -ItemType Directory
-$path3 = New-Item -Path "C:\Users\user\Desktop\$folder3" -ItemType Directory
+New-Item -Path "Shell", "Scripting", "Task" -ItemType Directory
 
-$newfolders = ($path2+"\Task\"), ($path2+"\One\"), ($path2+"\Submit\")
+cd Shell
 
-Remove-Item -Path "C:\Users\user\Desktop\$folder1\*"
-Remove-Item -Path "C:\Users\user\Desktop\$folder2\*"
+New-Item -Path "HyperionDev", "Powershell", "New Folder" -ItemType Directory
+
+
+cd ..
+
+Remove-Item -Path "Scripting", "Task"
--- Shell Scripting Tasks/ifExample.ps1
+++ Shell Scripting Tasks/ifExample.ps1
@@ -1,19 +1,14 @@
-$newFolder = "C:\Users\Folder\new_folder"
-$ifFolder = "C:\Users\Folder\if_folder"
-
-if (Test-Path -Path $newFolder -PathType) {
-    Write-Putput "The folder 'new_folder' already exists. Creating 'if_folder'."
-
-    New-Item -Path $ifFolder -ItemType Directory
-    Write-Output "Folder 'if_folder' created."
+if (Test-Path -Path "C:\Users\User\Hyperion\ShellScripting\new_folder" -ItemType Directory) {
+    New-Item -Path "C:\Users\User\Hyperion\ShellScripting\if_folder" -ItemType Directory
+    Write-Output "'new_folder' exists. 'if_folder' created."
 } else {
-    Write-Output "The folder 'new_folder' does not exist. 'if_folder' not created."
+    Write-Output "'new_folder' does not exist.'if_folder' not created."
 }
 
-if (Test-Path -Path ".$ifFolder") {
-    New-Item -Path ".\hyperionDev" -ItemType Directory
-    Write-Output "if_folder found. Create hyperionDev."
+if (Test-Path -Path "C:\Users\User\Hyperion\ShellScripting\if_folder" -ItemType Directory) {
+    New-Item -Path "C:\Users\User\Hyperion\ShellScripting\hyperionDev" -ItemType Directory
+    Write-Output "'if_folder' exists. 'hyperionDev' created."
 } else {
-    New-Item -Path ".$newFolder" -ItemType Directory
-    Write-Output "if_folder not found. Create new-projects."
+    New-Item -Path "C:\Users\User\Hyperion\ShellScripting\new-projects" -ItemType Directory
+    Write-Output "'if_folder' does not exist. 'new-projects' created."
 }
