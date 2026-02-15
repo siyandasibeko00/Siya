6608b9fa2f3d43781f7b6905aa3974ade9dd602d
Siya Sibeko <siyandasibeko00@gmail.com>
2026-02-15 21:18:05 +0200

    Add files via upload

--- Shell Scripting Tasks/ifExample.ps1
+++ Shell Scripting Tasks/ifExample.ps1
@@ -1,14 +1,14 @@
-if (Test-Path -Path "C:\Users\User\Hyperion\ShellScripting\new_folder" -ItemType Directory) {
-    New-Item -Path "C:\Users\User\Hyperion\ShellScripting\if_folder" -ItemType Directory
+if (Test-Path -Path ".\new_folder" -ItemType Directory) {
+    New-Item -Path ".\if_folder" -ItemType Directory
     Write-Output "'new_folder' exists. 'if_folder' created."
 } else {
     Write-Output "'new_folder' does not exist.'if_folder' not created."
 }
 
-if (Test-Path -Path "C:\Users\User\Hyperion\ShellScripting\if_folder" -ItemType Directory) {
-    New-Item -Path "C:\Users\User\Hyperion\ShellScripting\hyperionDev" -ItemType Directory
+if (Test-Path -Path ".\if_folder" -ItemType Directory) {
+    New-Item -Path ".\hyperionDev" -ItemType Directory
     Write-Output "'if_folder' exists. 'hyperionDev' created."
 } else {
-    New-Item -Path "C:\Users\User\Hyperion\ShellScripting\new-projects" -ItemType Directory
+    New-Item -Path ".new-projects" -ItemType Directory
     Write-Output "'if_folder' does not exist. 'new-projects' created."
 }
